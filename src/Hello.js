f434a36f76bbc7224338663f39de4c129541a7a0
Abok Isaac <abokisaac@gmail.com>
2018-10-18 10:14:17 +0300

    Hello word

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Hello.css'
+
+const Hello = (props) => {
+    return (
+      <div className="f1 tc">
+        <h1> { props.greeting } </h1>
+        <p>Welcome to reactjs</p>
+      </div>
+    )
+}
+
+export default Hello
